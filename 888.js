941cf98214fde9fd25a27949cf4a6afec555c3d3
Bradley7776 <103395186+Bradley7776@users.noreply.github.com>
2022-04-19 13:10:33 +0800

    Add files via upload

---
+++ 888.js
@@ -0,0 +1,7 @@
+
+function sum() {
+ var x = document.getElementById("Text1").value;
+ var y = document.getElementById("Text2").value;
+ var z = Number(x) + Number(y)
+ document.getElementById("demo").innerHTML = z  ;
+}
